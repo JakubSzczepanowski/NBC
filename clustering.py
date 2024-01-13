6023a9ee1677eea506f54154b7a115c1676c7369
Jakub Szczepanowski <jakub.szczepanowski.rad@gmail.com>
2024-01-13 20:08:18 +0100

    Testing

--- clustering.py
+++ clustering.py
@@ -10,8 +10,6 @@ class NBC:
     def __init__(self, l: int = 2, method: Literal['normal', 'optimized', 'heuristic'] = 'normal'):
         self.l = l
         self.method = method
-        self.pesimistic_distance_cache = {}
-        self.real_distance_cache = {}
 
     def fit(self, X: pd.DataFrame):
         self.n = len(X.index)
@@ -25,30 +23,31 @@ class NBC:
             ]
         elif self.method == 'optimized':
             self.distances = cdist(X, X, 'minkowski', p=self.l)
+        elif self.method == 'heuristic':
+            X['distance_with_r'] = 0.0
+            r = X.min()
+
+            for index, row in X.iterrows():
+                X.at[index, 'distance_with_r'] = self.distance(row, r, self.l)
+
+            X = X.sort_values(by='distance_with_r')
 
     def predict(self, X: pd.DataFrame, k: int):
         
         n = len(X.index)
-        kNN_counter = np.zeros(n)
-        RkNN_counter = np.zeros(n)
+        kNN_counter = np.zeros(n, dtype=np.int32)
+        RkNN_counter = np.zeros(n, dtype=np.int32)
         clst_no = np.full(n, -1)
         neighbours = []
         for i in range(n):
-            indices = self._find_neighbors(i, k)
+            indices = self._find_neighbors(i, k, X)
 
-            kNN_counter[i] = len(indices)
+            counter = len(indices)
+            kNN_counter[i] = np.finfo(float).eps if counter == 0 else counter
             for neighbour_index in indices:
                 RkNN_counter[neighbour_index] += 1
             neighbours.append(indices)
-        ndf = RkNN_counter/kNN_counter
-
-        # ndf = np.load('ndf.npy')
-
-        # data = ''
-        # with open('neighbours.csv', 'r') as f:
-        #     data = f.read()
-        # csv_reader = csv.reader(data.splitlines())
-        # neighbours = [list(map(int, row)) for row in csv_reader]
+        ndf = RkNN_counter//kNN_counter
 
         cluster_count = 0
         DPSet = queue.Queue()
@@ -70,66 +69,11 @@ class NBC:
             cluster_count += 1
         return clst_no
 
-        # X['distance_with_r'] = 0.0
-        # self.l = l
-
-        # for index, row in X.iterrows():
-        #     X.at[index, 'distance_with_r'] = self.distance(row, r, l)
-
-        # X = X.sort_values(by='distance_with_r')
-
-        # n = len(X.index)
-        # for i in range(n):
-        #     left_index = max(0, i - self.k)
-        #     right_index = min(n, i + self.k + 1)
-
-        #     target_series = X.iloc[i]
-        #     indexed_values = [[j, X.iloc[j], 0] for j in range(left_index, right_index) if j != i]
-        #     sorted_indexes = sorted(indexed_values, key=lambda x: self._pesimistic_cache_check_or_save(x[1], target_series))
-        #     candidates = sorted_indexes[:self.k]
-        #     eps = 0
-        #     for index, c in enumerate(candidates):
-        #         real_distance = self._real_cache_check_or_save(c[1], target_series)
-        #         candidates[index][2] = real_distance
-        #         if real_distance > eps:
-        #             eps = real_distance
-        #     inner_left_index = min(candidates, key=lambda x: x[0])[0] - 1
-        #     if inner_left_index == i:
-        #         inner_left_index -= 1
-        #     inner_right_index = max(candidates, key=lambda x: x[0])[0] + 1
-        #     if inner_right_index == i:
-        #         inner_right_index -= 1
-        #     up_run = True
-        #     down_run = True
-        #     while up_run or down_run:
-        #         if inner_right_index < n:
-        #             record = X.iloc[inner_right_index]
-        #             greater_than_eps, new_eps = self._compare_with_epsilon(eps, record, target_series)
-        #             if greater_than_eps:
-        #                 down_run = False
-        #             else:
-        #                 eps = new_eps
-        #                 candidates = [c for c in candidates if c[2] <= new_eps]
-        #                 candidates.append((inner_right_index, record, new_eps))
-        #             inner_right_index += 1
-        #         elif down_run:
-        #             down_run = False
-        #         if inner_left_index >= 0:
-        #             record = X.iloc[inner_left_index]
-        #             greater_than_eps, new_eps = self._compare_with_epsilon(eps, record, target_series)
-        #             if greater_than_eps:
-        #                 up_run = False
-        #             else:
-        #                 candidates = [c for c in candidates if c[2] <= new_eps]
-        #                 candidates.append((inner_right_index, record, new_eps))
-        #                 eps = max(candidates, key=lambda x: x[2])
-        #             inner_left_index -= 1
-        #         elif up_run:
-        #             up_run = False
-        #     print(candidates)
+        
     
-    def _find_neighbors(self, current_element: int, k: int):
+    def _find_neighbors(self, current_element: int, k: int, X: pd.DataFrame):
         indices = None
+
         if self.method == 'normal':
             row_distances = self.distances[current_element]
             row_distances.sort(key=lambda x: x[1])
@@ -138,8 +82,11 @@ class NBC:
             # Sprawdzenie, czy istnieją inne punkty równej odległości
             equal_distance_indices = [index for index, dist in row_distances[k+1:] if dist == row_distances[k][1]]
             indices.extend(equal_distance_indices)
+
         elif self.method == 'optimized':
             indices = np.argsort(self.distances[current_element])[1:k+1]
+            if len(indices) == 0:
+                return indices
             max_distance = self.distances[current_element, indices[-1]]
 
             # Sprawdzenie, czy istnieją inne punkty równej odległości
@@ -148,18 +95,76 @@ class NBC:
             # Dodaj indeksy o równej odległości do listy
             equal_distance_indices = np.where(equal_distance_mask)[0]
             indices = np.concatenate((indices, equal_distance_indices))
+
+        elif self.method == 'heuristic':
+
+            for i in range(self.n):
+                left_index = max(0, i - k)
+                right_index = min(self.n, i + k + 1)
+
+                target_series = X.iloc[i]
+                indexed_values = [[j, X.iloc[j], 0] for j in range(left_index, right_index) if j != i]
+                sorted_indexes = sorted(indexed_values, key=lambda x: np.abs(x[1]['distance_with_r'] - target_series['distance_with_r']))
+                candidates = sorted_indexes[:k]
+                eps = 0
+                for index, c in enumerate(candidates):
+                    real_distance = self.distance(c[1].drop('distance_with_r', errors='ignore'), target_series.drop('distance_with_r', errors='ignore'), self.l)
+                    candidates[index][2] = real_distance
+                    if real_distance > eps:
+                        eps = real_distance
+                inner_left_index = min(candidates, key=lambda x: x[0])[0] - 1
+                if inner_left_index == i:
+                    inner_left_index -= 1
+                inner_right_index = max(candidates, key=lambda x: x[0])[0] + 1
+                if inner_right_index == i:
+                    inner_right_index -= 1
+                up_run = True
+                down_run = True
+                while up_run or down_run:
+                    if inner_right_index < self.n and down_run:
+                        record = X.iloc[inner_right_index]
+                        pesimistic_distance = np.abs(record['distance_with_r'] - target_series['distance_with_r'])
+                        if pesimistic_distance >= eps:
+                            down_run = False
+                        else:
+                            real_distance = self.distance(record.drop('distance_with_r', errors='ignore'), target_series.drop('distance_with_r', errors='ignore'), self.l)
+                            if real_distance < eps:
+                                same_size_elements = [elem[0] for elem in candidates if elem[2] == eps]
+                                if len(candidates) - len(same_size_elements) >= k - 1:
+                                    candidates = [elem for elem in candidates if elem[0] not in same_size_elements]
+                                    candidates.append((inner_right_index, record, real_distance))
+                                    eps = max(candidates, key=lambda x: x[2])[2]
+                                else:
+                                    candidates.append((inner_right_index, record, real_distance))
+                            elif real_distance == eps:
+                                candidates.append((inner_right_index, record, real_distance))
+                        inner_right_index += 1
+
+                    if inner_left_index >= 0 and up_run:
+                        record = X.iloc[inner_left_index]
+                        pesimistic_distance = np.abs(record['distance_with_r'] - target_series['distance_with_r'])
+                        if pesimistic_distance >= eps:
+                            down_run = False
+                        else:
+                            real_distance = self.distance(record.drop('distance_with_r', errors='ignore'), target_series.drop('distance_with_r', errors='ignore'), self.l)
+                            if real_distance < eps:
+                                same_size_elements = (elem[0] for elem in candidates if elem[2] == eps)
+                                if len(candidates) - len(same_size_elements) >= k - 1:
+                                    candidates = [elem for elem in candidates if elem[0] not in same_size_elements]
+                                    candidates.append((inner_right_index, record, real_distance))
+                                    eps = max(candidates, key=lambda x: x[2])[2]
+                                else:
+                                    candidates.append((inner_right_index, record, real_distance))
+                            elif real_distance == eps:
+                                candidates.append((inner_right_index, record, real_distance))
+                        inner_left_index -= 1
+                print(candidates)
         return indices
 
     def _compare_with_epsilon(self, eps: float, current_series: pd.Series, target_series: pd.Series) -> tuple[bool, float]:
 
         greater_than_eps = False
-        pesimistic_distance = self._pesimistic_cache_check_or_save(current_series, target_series)
-        if pesimistic_distance >= eps:
-            greater_than_eps = True
-        else:
-            real_distance = self._real_cache_check_or_save(current_series, target_series)
-            if real_distance < eps:
-                eps = real_distance
+        
 
         return (greater_than_eps, eps)
 
--- testing.py
+++ testing.py
@@ -16,3 +16,35 @@ def find_best_predictions(X: pd.DataFrame, y: np.ndarray, model: clustering.NBC,
         rand_values.append(adjusted_rand_score(model.predict(X, k), y))
     plt.plot(k_values, rand_values)
     return k_values[np.argmax(rand_values)], np.max(rand_values)
+
+from concurrent.futures import ProcessPoolExecutor
+
+def compute_rand_value(args):
+    X, y, model, k = args
+    predictions = model.predict(X, k)
+    return adjusted_rand_score(predictions, y)
+
+def find_best_predictions_async(X: pd.DataFrame, y: np.ndarray, model: clustering.NBC, max_range=100, n_jobs=4) -> tuple[int, float]:
+
+    k_values = np.arange(5, max_range + 1, step=5)
+
+    # Używamy ProcessPoolExecutor do równoległego przetwarzania
+    args_list = [(X, y, model, k) for k in k_values]
+    with ProcessPoolExecutor(max_workers=n_jobs) as executor:
+        rand_values = list(executor.map(compute_rand_value, args_list))
+
+    best_k = k_values[np.argmax(rand_values)]
+    k_values = np.arange(best_k - 5, best_k + 6)
+
+    # Ponowne obliczenia dla wąskiego zakresu
+    args_list = [(X, y, model, k) for k in k_values]
+    with ProcessPoolExecutor(max_workers=n_jobs) as executor:
+        rand_values = list(executor.map(compute_rand_value, args_list))
+
+    plt.plot(k_values, rand_values)
+    plt.xlabel('k')
+    plt.ylabel('Adjusted Rand Index')
+    plt.title('Adjusted Rand Index for Different k Values')
+    plt.show()
+
+    return k_values[np.argmax(rand_values)], np.max(rand_values)
